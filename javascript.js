9aaeeaba104a45d8cdbd2794afa8857f3f18e074
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-09-29 16:22:04 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -90,5 +90,52 @@ title.style.color = "black";
 title.style.background = "Grey"
 // We can give a variable in which a id or class will be stored later we will perform the task through declared variable.
 ------------------------------------------------------------------------------------------------------------------------
-  
+---------------------------------------------- Add Event Listener ------------------------------------------------------
+mybtn.addEventListener('click', function() {
+    title.innerHTML = my_textbox.value;
+})
+// here in Event Listener we can change upper text value by changing the value in the button or box.
+// Example ---> index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Add Event Listener</title>
+</head>
+<body>
+    <h2 id="title">Welcome to this page</h2>
 
+    <button id="btn1">Sign-in</button>
+    <button id="btn2">Sign-up</button>
+    <button id="btn3">Sign-out</button>
+    <button id="reset">Reset</button>
+
+    <script src="app.js"></script>
+</body>
+</html>
+// ----> app.js
+let title = document.querySelector("#title");
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#reset");
+
+btn1.addEventListener('click', function() {
+    title.innerHTML = "here we goo!";
+})
+
+btn2.addEventListener('click', function() {
+    title.innerHTML = "here we goooo!";
+})
+
+btn3.addEventListener('click', function() {
+    title.innerHTML = "here we gooooooooo!";
+})
+
+btn4.addEventListener('click', function(){
+    title.innerHTML = "Welcome to this page";
+})
+// whenever you will click on button Sign-in txt will be changed in title section nut when you will click on reset it will change if value to original.
+------------------------------------------------------------------------------------------------------------------------
