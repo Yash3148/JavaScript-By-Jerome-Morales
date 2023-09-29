66898b6d41847d03905154e6e6d8fbdf9b28fb19
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-09-29 20:41:51 +0530

    app.js

---
+++ Create Element/Type2/app.js
@@ -0,0 +1,16 @@
+let mytext = document.querySelector("#mytext");
+let btn = document.querySelector("#btn");
+let mylist = document.querySelector("#mylist");
+
+btn.addEventListener('click', function(){
+    let entered_data = mytext.value;
+
+    if (entered_data.trim() == ""){
+        alert("Please enter data.");
+    }else{
+
+        let new_elem = document.createElement("li");
+        new_elem.innerHTML = mytext.value;
+        mylist.appendChild(new_elem);
+    }
+})
