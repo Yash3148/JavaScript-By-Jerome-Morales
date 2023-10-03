e26cf2b8acd5d5a842cfc47ea752139140243f7e
Yash <yashsingh3148@gmail.com>
2023-10-03 18:53:11 +0530

    First Commit

---
+++ Increment_Decrement/app.js
@@ -0,0 +1,6 @@
+let numberbox = document.querySelector("#numberbox");
+const minus1 = document.querySelector("#minus");
+const plus1 = document.querySelector("#plus");
+const reset = document.querySelector("#reset");
+
+plus1
