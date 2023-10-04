1fd4edacc1fabdf44ff7a254ccb880666e15ba08
Yash <yashsingh3148@gmail.com>
2023-10-04 11:28:57 +0530

    First Commit

--- Increment_Decrement/app.js
+++ Increment_Decrement/app.js
@@ -3,4 +3,27 @@ const minus1 = document.querySelector("#minus");
 const plus1 = document.querySelector("#plus");
 const reset = document.querySelector("#reset");
 
-plus1
+let my_number=0;
+
+plus1.addEventListener('click', function (){
+    // my_number = my_number + 1;
+    my_number++;
+    numberbox.value = my_number;
+})
+
+// minus1.addEventListener('click', function (){
+//     my_number = my_number - 1;
+//     numberbox.value = my_number;
+// })
+
+     //This Function is known as Arrow Function.   
+minus1.addEventListener('click', () => {
+    //my_number = my_number - 1;
+    my_number--;
+    numberbox.value = my_number;
+})
+
+reset.addEventListener('click', function (){
+    my_number = 0;
+    numberbox.value = my_number;
+})
