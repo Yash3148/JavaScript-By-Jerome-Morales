5905328c88303495c91f86e89027a53628be0326
Yash <yashsingh3148@gmail.com>
2023-10-04 15:04:10 +0530

    First Commit

---
+++ Loops/Loops-Excercise/app.js
@@ -0,0 +1,10 @@
+// Ques.1 Generate numbers from one to ten using console. If the number is even display the text "even" else just console.log the number
+for (let x = 1; x <= 10; x++){
+    if(x % 2 == 0){
+        console.log("Even");
+    }else{
+        console.log(x);
+    }
+}
+
+
