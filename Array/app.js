a6d0b0fb0a2c80db2c69d9aaf129510d05df0ff0
Yash <yashsingh3148@gmail.com>
2023-10-04 14:01:15 +0530

    First Commit

--- Array/app.js
+++ Array/app.js
@@ -2,21 +2,43 @@ let displayArea = document.querySelector("#displayArea");
 let inputBox = document.querySelector("#inputBox");
 let myBtn = document.querySelector("#myBtn");
 
-let fruits = ["apple", "banana", "berry"];
+// WHILE LOOP
 
-// fruits.push("Orange");
-console.log(fruits);
-displayArea.innerHTML = fruits[3];
+// let x = 0 ;
 
-fruits.pop();   // Pop is used to remove last string in the list. 
-fruits.shift();  // Whearas shift is used to remove First string in the list.
-fruits.unshift("Corn"); // Unshift is used to add an item to the beginning of an array.
-fruits.push("Mango"); // Push is used to add an item to the ending of an array.
+// while (x < 100){
+//     x++;
+//     console.log(x);
+// }
 
-myBtn.addEventListener("click", function () {
-    // let fruit1 = "Apple";
-    // let fruit2 = "Banana";
-    // let fruit3 = "Berry";
-    // displayArea.innerHTML = fruit2;
+// DO WHILE LOOP
+// let x = 0 ;
+// do {
+//     x++;
+//     console.log(x);
 
-})
+// }while(x < 4);
+
+// FOR LOOP 
+
+
+
+
+// let fruits = ["Apple", "Banana"];
+// displayArea.innerHTML = fruits;
+
+// // fruits.push("Orange");
+// // console.log(fruits);
+// // displayArea.innerHTML = fruits[3];
+
+// // fruits.pop();   // Pop is used to remove last string in the list. 
+// // fruits.shift();  // Whearas shift is used to remove First string in the list.
+// // fruits.unshift("Corn"); // Unshift is used to add an item to the beginning of an array.
+// // fruits.push("Mango"); // Push is used to add an item to the ending of an array.
+
+// myBtn.addEventListener("click", function () {
+//     let new_fruit = inputBox.value;
+//     fruits.push(new_fruit);
+//     displayArea.innerHTML = fruits;
+
+// })
