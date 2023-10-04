403342c440ce83afe7100a262a11b9529d9716ad
Yash <yashsingh3148@gmail.com>
2023-10-04 14:13:13 +0530

    First Commit

--- Array/app.js
+++ Array/app.js
@@ -2,28 +2,6 @@ let displayArea = document.querySelector("#displayArea");
 let inputBox = document.querySelector("#inputBox");
 let myBtn = document.querySelector("#myBtn");
 
-// WHILE LOOP
-
-// let x = 0 ;
-
-// while (x < 100){
-//     x++;
-//     console.log(x);
-// }
-
-// DO WHILE LOOP
-// let x = 0 ;
-// do {
-//     x++;
-//     console.log(x);
-
-// }while(x < 4);
-
-// FOR LOOP 
-
-
-
-
 // let fruits = ["Apple", "Banana"];
 // displayArea.innerHTML = fruits;
 
---
+++ Loops/app.js
@@ -0,0 +1,22 @@
+let displayArea = document.querySelector("#displayArea");
+let inputBox = document.querySelector("#inputBox");
+let myBtn = document.querySelector("#myBtn");
+
+// WHILE LOOP
+
+// let x = 0 ;
+
+// while (x < 100){
+//     x++;
+//     console.log(x);
+// }
+
+// DO WHILE LOOP
+// let x = 0 ;
+// do {
+//     x++;
+//     console.log(x);
+
+// }while(x < 4);
+
+// FOR LOOP 
