eab36a32873be2f2b3d8e1e2e484fe2b4b7421a5
Yash <yashsingh3148@gmail.com>
2023-10-04 14:44:59 +0530

    First Commit

--- Loops/app.js
+++ Loops/app.js
@@ -2,21 +2,40 @@ let displayArea = document.querySelector("#displayArea");
 let inputBox = document.querySelector("#inputBox");
 let myBtn = document.querySelector("#myBtn");
 
-// WHILE LOOP
+let languages = ["Javascript", " PHP", " Python"];
+
+for(let language of languages){
+    let li = document.createElement("li");
+    li.innerHTML = language;
+    displayArea.appendChild(li);
+}
+
+myBtn.addEventListener('click', function(){
+    let new_item = inputBox.value;
+    languages.push(new_item);
+
+    let li = document.createElement("li");
+    li.innerHTML = new_item;
+    displayArea.appendChild(li);
+})
+
+                               // WHILE LOOP
 
 // let x = 0 ;
-
 // while (x < 100){
 //     x++;
 //     console.log(x);
 // }
 
-// DO WHILE LOOP
+                               // DO WHILE LOOP
 // let x = 0 ;
 // do {
 //     x++;
 //     console.log(x);
-
 // }while(x < 4);
 
-// FOR LOOP 
+                                // FOR LOOP 
+
+// for (let x = 1; x < 11; x++){
+//     console.log(x);
+// }
