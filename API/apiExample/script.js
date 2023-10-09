76427757e0c2f9ce08ab4ca1da3a621609146f94
Yash <yashsingh3148@gmail.com>
2023-10-09 12:50:40 +0530

    First Commit

--- API/apiExample/script.js
+++ API/apiExample/script.js
@@ -1,13 +1,16 @@
-fetch ('data.txt').then((response) => {
+let container = document.querySelector("#container");
+
+fetch ('https://swapi.dev/api/people/1/').then((response) => {
     return response.text()
 }).then((data) => {
     const myData = JSON.parse(data);
     console.log(myData);
 
-    console.log(myData[1].name);
-    console.log(myData[1].age);
-    console.log(myData[1].city);
+    console.log(myData);
 
+    let p = document.createElement('p');
+    p.innerHTML = myData.name;
+    container.appendChild(p);
 
 }).catch((error) => {
     console.log('Something went wrong: ', error);
