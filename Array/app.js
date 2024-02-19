f7ed10cfff801527f0465e9f4eaf4e5158304edf
Yash <yashsingh3148@gmail.com>
2024-02-19 14:22:04 +0530

    Added

--- Array/app.js
+++ Array/app.js
@@ -2,21 +2,20 @@ let displayArea = document.querySelector("#displayArea");
 let inputBox = document.querySelector("#inputBox");
 let myBtn = document.querySelector("#myBtn");
 
-// let fruits = ["Apple", "Banana"];
-// displayArea.innerHTML = fruits;
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Red"];
+ displayArea.innerHTML = fruits;
 
-// // fruits.push("Orange");
-// // console.log(fruits);
-// // displayArea.innerHTML = fruits[3];
+    fruits.push("Orange");
+    console.log(fruits);
+    // displayArea.innerHTML = fruits[3];
 
-// // fruits.pop();   // Pop is used to remove last string in the list. 
-// // fruits.shift();  // Whearas shift is used to remove First string in the list.
-// // fruits.unshift("Corn"); // Unshift is used to add an item to the beginning of an array.
-// // fruits.push("Mango"); // Push is used to add an item to the ending of an array.
+    // fruits.pop();   // Pop is used to remove last string in the list. 
+    // fruits.shift();  // Whearas shift is used to remove First string in the list.
+    // fruits.unshift("Corn"); // Unshift is used to add an item to the beginning of an array.
+    // fruits.push("Mango"); // Push is used to add an item to the ending of an array.
 
-// myBtn.addEventListener("click", function () {
-//     let new_fruit = inputBox.value;
-//     fruits.push(new_fruit);
-//     displayArea.innerHTML = fruits;
-
-// })
+ myBtn.addEventListener("click", function () {
+     let new_fruit = inputBox.value;
+     fruits.push(new_fruit);
+     displayArea.innerHTML = fruits;
+ })
